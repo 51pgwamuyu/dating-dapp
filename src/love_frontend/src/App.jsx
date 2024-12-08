0cfea2e646fe1c5f550680cdfebdffd21be0ec17
51pgwamuyu <elizabethwakarima51@gmail.com>
2024-12-08 23:15:17 +0300

    first commit

---
+++ src/love_frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import NavBar from './components/navabar';
+//import { love_backend } from 'declarations/love_backend';
+import {Routes,BrowserRouter, Route} from "react-router-dom"
+import Home from './components/Home';
+function App() {
+  // const [greeting, setGreeting] = useState('');
+
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const name = event.target.elements.name.value;
+  //   love_backend.greet(name).then((greeting) => {
+  //     setGreeting(greeting);
+  //   });
+  //   return false;
+  // }
+
+  return (
+   <BrowserRouter>
+   <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+   </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/love_frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./navabar";
+
+const Home = () => {
+  return (
+    <div className=" h-[70vh] w-full flex justify-center items-center ">
+      <div className=" -">
+        <button>Login with identity</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
